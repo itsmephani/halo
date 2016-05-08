6b0f3e0a1e391f8d2d104e7277f8641c9d69a061
phani <vs3.phani@gmail.com>
2016-05-08 22:36:52 +0530

    first commit

---
+++ db/migrate/20160408085551_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string   "first_name"
+      t.string   "last_name"
+      t.string   "email"
+      t.text     "password_digest"
+      t.string   "state"
+      t.string   "country",                limit: 75
+      t.string   "gender",                 limit: 6
+      t.string   "mobile_number",          limit: 16
+      t.string   "access_token",           limit: 50
+      t.string   "platform"
+      t.string   "phone_type"
+      t.string   "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.string   "provider"
+
+      t.timestamps
+    end
+  end
+end
