7401a69f09bd1114d2482c8c9a5dc5772cccb187
phani <vs3.phani@gmail.com>
2016-10-17 21:48:00 +0530

    add seed of 5 users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  {name: 'John', email: 'john@example.com'},
+  {name: 'Greg', email: 'greg@example.com'},
+  {name: 'Ken', email: 'ken@example.com'},
+  {name: 'Tony', email: 'tony@example.com'},
+  {name: 'Phani', email: 'phani@example.com'}
+]
+
+users.each do |user|
+  user['password'] = 'test1234'
+  user['password_confirmation'] = 'test1234'
+  User.create(user)
+  puts "create users with name: #{user[:name]}, password: #{user['password']}"
+end
+
